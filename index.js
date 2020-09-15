8136bad8ee328f77d48efa3fc8955bf905f7da0a
islommashanlo <islom.mashanlo@gmail.com>
2020-09-14 23:40:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+let messages = []
+ 
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+ 
+  return messages;
+}
+ 
+function countDown(i) {
+while (i > 0) {
+    console.log(i);
+    i -= 1;
+    }
+    console.log( i )
+}
